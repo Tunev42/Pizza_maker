396f0a6144a47e5cf0ae5c5071bb23ffb34184c1
Tunev42 <kostik17raven@gmail.com>
2025-11-22 15:26:08 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 names = []
 ages = []
 order = []
+size_pizza_18 = []
+size_pizza_17 = []
+pizza_18 = []
+pizza_17 = []
 total_price = []
-print("Введите ваше имя")
-def name():
-    name = input()
-    names.append(name)
-name()
-print(names)
-print("Какой ваш возврост?")
-def age():
-    while True:
-        age = int(input())
-        if age <= 17:
-            print("У вас будет неполненное меню из-за \n"
-                  "того что вам ещё не исполнилось 18 лет \n")
-            ages.append(age)
-            break
-        elif age >= 18:
-            print("У вас полное меню")
-            ages.append(age)
-            break
-        else:
-            print("Введите сколько вам лет")
-age()
-def menu():
+
+def menu_18(): #в этой функции мы создаём меню 18+
     print("Вы хотите заказать уже созданую или свою")
     while True:
         menu = input()
-        if menu == "созданую":
+        if menu == "созданую": # тут запрашивается созданные пиццы или свою, тоесть создать свою
             print("Выберите какую: \n"
                   "Пеперони: \n"
                   "Пеперони, сыр(на выбор), кетчуп и добавки на выбор \n"
@@ -36,34 +19,99 @@ def menu():
                   "Ананасы, кетчуп, сыр(на выбор) и добавки на выбор.\n"
                   "Сырная: \n"
                   "Сыр(на выбор), кутчуп и добавки на выбор. \n")
-            menu2 = input().lower()
             while True:
+                menu2 = input()
                 if menu2 == "пеперони":
+                    while True:
+                        print("Какой размер вы хотите 15, 20, 25 или 30")
+                        size_18 = int(input())
+                        if size_18 == 15:
+                            size_pizza_18.append(size_18)
+                            break
+                        elif size_18 == 20:
+                            size_pizza_18.append(size_18)
+                            break
+                        elif size_18 == 25:
+                            size_pizza_18.append(size_18)
+                            break
+                        elif size_18 == 30:
+                            size_pizza_18.append(size_18)
+                            break
+                        else:
+                            print("Введите заново")
+                            continue
                     order.append(menu2)
-                    break
                 elif menu2 == "гавайская":
+                    while True:
+                        print("Какой размер вы хотите 15, 20, 25 или 30")
+                        size_18 = int(input())
+                        if size_18 == 15:
+                            size_pizza_18.append(size_18)
+                            break
+                        elif size_18 == 20:
+                            size_pizza_18.append(size_18)
+                            break
+                        elif size_18 == 25:
+                            size_pizza_18.append(size_18)
+                            break
+                        elif size_18 == 30:
+                            size_pizza_18.append(size_18)
+                            break
+                        else:
+                            print("Введите заново")
+                            continue
                     order.append(menu2)
                     break
                 elif menu2 == "сырная":
+                    while True:
+                        print("Какой размер вы хотите 15, 20, 25 или 30")
+                        size_18 = int(input())
+                        if size_18 == 15:
+                            size_pizza_18.append(size_18)
+                            break
+                        elif size_18 == 20:
+                            size_pizza_18.append(size_18)
+                            break
+                        elif size_18 == 25:
+                            size_pizza_18.append(size_18)
+                            break
+                        elif size_18 == 30:
+                            size_pizza_18.append(size_18)
+                            break
+                        else:
+                            print("Введите заново")
+                            continue
                     order.append(menu2)
                     break
                 else:
                     print("Введите 'пеперони', 'гавайская' или 'сырная'")
+                    continue
             break
         elif menu == "свою":
-            print("Какой размер(15см, 20см, 25см, 30см, 35см) \n")
-            def size_pizza():
-                size_pizza = int(input())
-                if size_pizza == 15:
-                    print("Какие вы хотитите ингридиенты?")
-                elif size_pizza == 20:
-                    print("Какие вы хотитите ингридиенты?")
-                elif size_pizza == 25:
-                    print("Какие вы хотитите ингридиенты?")
-                elif size_pizza == 30:
-                    print("Какие вы хотитите ингридиенты?")
-                elif size_pizza == 35:
-                    print("Какие вы хотитите ингридиенты?")
+            def size_pizza(): #в этой функции срашивается о размере пиццы
+                while True:
+                    print("Какой размер(15см, 20см, 25см, 30см, 35см) \n")
+                    size_pizza = int(input())
+                    if size_pizza == 15:
+                        print("Какие вы хотитите ингридиенты?")
+                        size_pizza_18.append(size_pizza)
+                        break
+                    elif size_pizza == 20:
+                        print("Какие вы хотитите ингридиенты?")
+                        size_pizza_18.append(size_pizza)
+                        break
+                    elif size_pizza == 25:
+                        print("Какие вы хотитите ингридиенты?")
+                        size_pizza_18.append(size_pizza)
+                        break
+                    elif size_pizza == 30:
+                        print("Какие вы хотитите ингридиенты?")
+                        break
+                    elif size_pizza == 35:
+                        print("Какие вы хотитите ингридиенты?")
+                        size_pizza_18.append(size_pizza)
+                        break
+
             size_pizza()
             def custom_pizza():
                     print(      "Выберите ингридиенты: \n"
@@ -89,21 +137,86 @@ def menu():
                         fl = False
                         print("Хороший выбор")
             custom_pizza()
-menu()
-def drinks():
-    print("Вы хотите напити?")
+def menu_17():
+    print("У вас сокращённое меню")
+    fl = True
+    while fl:
+        print("Какую пиццу вы хотите? \n"
+              "пеперони \n"
+              "гавайская \n"
+              "сырная \n")
+        menu_17_pizza = input()
+        if menu_17_pizza == "пеперони":
+            print("Какой размер пиццы")
+            while fl:
+                print("15 или 20 см")
+                size_17 = int(input())
+                if size_17 == 15:
+                    size_pizza_17.append(size_17)
+                    break
+                elif size_17 == 20:
+                    size_pizza_17.append(size_17)
+                    break
+                else:
+                    print("Введите заново")
+                    continue
+            break
+        elif menu_17_pizza == "гавайская":
+            print("Какой размер пиццы")
+            while fl:
+                size_17 = int(input())
+                print("15 или 20 см")
+                if size_17 == 15:
+                    size_pizza_17.append(size_17)
+                    break
+                elif size_17 == 20:
+                    size_pizza_17.append(size_17)
+                    break
+                else:
+                    print("Введите заново")
+                    continue
+        elif menu_17_pizza == "сырная":
+            print("Какой размер пиццы")
+            while fl:
+                size_17 = int(input())
+                print("15 или 20 см")
+                if size_17 == 15:
+                    size_pizza_17.append(size_17)
+                    break
+                elif size_17 == 20:
+                    size_pizza_17.append(size_17)
+                    break
+                else:
+                    print("Введите заново")
+                    continue
+print("Введите ваше фамилию, имя, отчество")
+def drinks_18():
+    print("Вы хотите напитки?(-ок)")
     drink = input()
+    cola = "кола"
+    juice = "сок"
+    sprite = "спрайт"
+    fanta = "фанта"
     f1 = True
     try:
         while f1:
             if drink == "да":
                 print("Какой напиток вы хотите: \n"
-                      "Кола, \n"
-                      "Сок, \n"
-                      "Спрайт, \n"
-                      "Фанта. \n")
-                vs = input()
-                order.append(vs)
+                      "Кола - 1, \n"
+                      "Сок - 2, \n"
+                      "Спрайт - 3, \n"
+                      "Фанта - 4 \n")
+                vs = int(input())
+                if vs == 1:
+                    order.append(cola)
+                elif vs == 2:
+                    order.append(juice)
+                elif vs == 3:
+                    order.append(sprite)
+                elif vs == 4:
+                    order.append(fanta)
+                else:
+                    print("Введите 1, 2, 3, или 4")
                 print("Вы хотите ещё напиток(-ки) 1-да 2- нет")
                 vs1 = int(input())
                 if vs1 == 1:
@@ -112,9 +225,73 @@ def drinks():
                     f1 = False
             else:
                 f1 = False
-    except "нет":
+    except 2:
         f1 = False
-drinks()
+def drinks_17():
+    print("Вы хотите напитки?(-ок)")
+    drinks_171 = input()
+    while True:
+        if drinks_171 == "да":
+            fl = True
+            try:
+                while fl:
+                    print("1 - сок \n"
+                          "2 - вода \n"
+                          "3 - минеральная вода \n"
+                          "4 - чай \n ")
+                    drinks_17w = input()
+                    while fl:
+                        if drinks_17w == "сок":
+                            order.append(drinks_17w)
+                            break
+                        elif drinks_17w == "вода":
+                            order.append(drinks_17w)
+                            break
+                        elif drinks_17w == "минеральная вода":
+                            order.append(drinks_17w)
+                            break
+                        elif drinks_17w == "чай":
+                            order.append(drinks_17w)
+                            break
+                        print("Вы хотите ещё напитки?")
+                        yesorno = input()
+                        if yesorno == "нет":
+                            fl = False
+                        elif yesorno == "да":
+                            fl = True
+            except "нет":
+                fl = False
+
+
+
+
+
+
+
+def name(): #в функции запрашивает имя
+    name = input()
+    names.append(name)
+name()
+print("Какой ваш возврост?")
+def age():
+    while True:
+        age = int(input())
+        if age <= 17:
+            print("У вас будет неполненное меню из-за \n"
+                  "того что вам ещё не исполнилось 18 лет \n")
+            ages.append(age)
+            menu_17()
+            drinks_17()
+            break
+        elif age >= 18:
+            print("У вас полное меню")
+            ages.append(age)
+            menu_18()
+            drinks_18()
+            break
+        else:
+            print("Введите сколько вам лет")
+age()
 print(order)
 while True:
     print("Всё верно?")
@@ -122,14 +299,14 @@ while True:
     if yes_no == "да":
         break
     elif yes_no == "нет":
-        print("Вы хотите что то удалить или добавить?")
+        print("Вы хотите что то удалить или добавить?(выберите продукт по счёту начиная от 0)")
         delsoradd = input()
         if delsoradd == "удалить":
             def dels():
                 try:
                     f2 = True
                     while f2:
-                        delses = input()
+                        delses = int(input())
                         order.remove(delses)
                         print("Вы хотите ещё чтото убрать?")
                         stop = input()
@@ -149,19 +326,16 @@ while True:
     else:
         print("Всё верно 'да' или 'нет' ")
         continue
-drinks()
 def mathorder():
-    ketchup = 100
-    chesse = 100
-    pineapples = 150
-    meat = 150
-    dough = 200
-    serfdom = 50
-    sprite = 100
-    coco_cola = 100
-    juice = 100
-    total_prices = order + ketchup + chesse + pineapples + meat + dough + serfdom + sprite + coco_cola + juice
-    total_price.append(total_prices)
+    f"ketchup: {100}"
+    f"chesse = {100}"
+    f"pineapples = {150}"
+    f"meat = {150}"
+    f"dough = {200}"
+    f"serfdom = {50}"
+    f"sprite = {100}"
+    f"coco_cola {100}"
+    f"juice = {100}"
+
     print(total_price)
 mathorder()
-
